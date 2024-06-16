9a92a8692e2c1e3d710eda5f4ac9876696f5829c
pis17629 <pistornadine@icloud.com>
2024-06-16 15:39:41 +0200

    UmleitungsURI geändert

--- app/msal.ts
+++ app/msal.ts
@@ -4,7 +4,7 @@ const msalInstance = new PublicClientApplication({
   auth: {
     clientId: "d38e52e7-0e64-46ff-a0ee-4848daa391bb",
     authority: "https://login.microsoftonline.com/0154176d-b837-49c5-b37e-251968ad316e",
-    redirectUri: "http://localhost:3000/", // Hier sollte die Umleitungs-URI deiner Anwendung stehen
+    redirectUri: "https://saltranslator.azurewebsites.net/", // Hier sollte die Umleitungs-URI deiner Anwendung stehen
   },
 });
 
